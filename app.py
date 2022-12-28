206adbef20b9eed077d548b1fd307d288244183a
zaidadianhua <2496344956@qq.com>
2022-12-28 17:17:09 +0800

    增加去雾功能

--- app.py
+++ app.py
@@ -5,7 +5,14 @@ import numpy as np
 import cv2
 from math import *
 from skimage.filters import gaussian
-
+from PIL import Image
+from models import FFA
+import torch
+import torch.nn as nn
+import torchvision.transforms as tfs
+import torchvision.utils as vutils
+import matplotlib.pyplot as plt
+from torchvision.utils import make_grid
 
 app = Flask(__name__)
 #---------------图片转换模块----------------
@@ -144,6 +151,177 @@ def noise(img):
         out[x, y, :] = 255
     return out
 
+# 雾化操作
+def Picture_defogging(haze):               #图片去雾
+    path_save = '../image01.jpg'
+    # parser=argparse.ArgumentParser()
+    # parser.add_argument('--task',type=str,default='its',help='its or ots')
+    # parser.add_argument('--test_imgs',type=str,default='test_imgs',help='Test imgs folder')
+    # opt=parser.parse_args()
+    # dataset=opt.task
+    dataset='ots'      #模型选择its或者ots
+    #print("dataset:",dataset)
+    gps=3
+    blocks=19
+    #img_dir='C:/Users/ASUS/Desktop/cl数字信号任务/FFA-Net-master/'
+    #print("img_dir:",img_dir)
+    #output_dir=abs+f'pred_FFA_{dataset}/'
+    #print("pred_dir:",output_dir)
+    #if not os.path.exists(output_dir):
+        #os.mkdir(output_dir)
+    # model_dir='./'+f'trained_models/{dataset}_train_ffa_{gps}_{blocks}.pk'
+    model_dir='D:/University Life/大三上/数字信号/课设/PictureTest/defogging/trained_models/ots_train_ffa_3_19.pk'
+    #print(model_dir)
+
+    device='cuda' if torch.cuda.is_available() else 'cpu'
+    ckp=torch.load(model_dir,map_location=device)
+    net=FFA(gps=gps,blocks=blocks)
+    net=nn.DataParallel(net)
+    net.load_state_dict(ckp['model'])
+    net.eval()
+    # for im in os.listdir(img_dir):
+    #     print(f'\r {im}',end='',flush=True)
+    #     haze = Image.open(img_dir+im)
+    #     haze1= tfs.Compose([
+    #         tfs.ToTensor(),
+    #         tfs.Normalize(mean=[0.64, 0.6, 0.58],std=[0.14,0.15, 0.152])
+    #     ])(haze)[None,::]
+    #     haze_no=tfs.ToTensor()(haze)[None,::]
+    #     with torch.no_grad():
+    #         pred = net(haze1)
+    #     ts=torch.squeeze(pred.clamp(0,1).cpu())
+    #     tensorShow([haze_no,pred.clamp(0,1).cpu()],['haze','pred'])
+    #     vutils.save_image(ts,output_dir+im.split('.')[0]+'_FFA.png')
+
+    haze1= tfs.Compose([
+        tfs.ToTensor(),
+        tfs.Normalize(mean=[0.64, 0.6, 0.58],std=[0.14,0.15, 0.152])
+    ])(haze)[None,::]
+    haze_no=tfs.ToTensor()(haze)[None,::]
+    with torch.no_grad():
+            pred = net(haze1)
+    ts=torch.squeeze(pred.clamp(0,1).cpu())
+
+    vutils.save_image(ts,path_save)
+    img = cv2.imread(path_save)
+    print(type(img))
+    return img
+def toTensor(img):
+    assert type(img) == np.ndarray,'the img type is {}, but ndarry expected'.format(type(img))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = torch.from_numpy(img.transpose((2, 0, 1)))
+    return img.float().div(255).unsqueeze(0)  # 255也可以改为256
+def tensorShow(tensors, titles=['haze']):
+    fig = plt.figure()
+    for tensor, tit, i in zip(tensors, titles, range(len(tensors))):
+        img = make_grid(tensor)
+        npimg = img.numpy()
+        ax = fig.add_subplot(221 + i)
+        ax.imshow(np.transpose(npimg, (1, 2, 0)))
+        ax.set_title(tit)
+    plt.show()
+def default_conv(in_channels, out_channels, kernel_size, bias=True):
+    return nn.Conv2d(in_channels, out_channels, kernel_size, padding=(kernel_size // 2), bias=bias)
+class PALayer(nn.Module):
+    def __init__(self, channel):
+        super(PALayer, self).__init__()
+        self.pa = nn.Sequential(
+            nn.Conv2d(channel, channel // 8, 1, padding=0, bias=True),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(channel // 8, 1, 1, padding=0, bias=True),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        y = self.pa(x)
+        return x * y
+class CALayer(nn.Module):
+    def __init__(self, channel):
+        super(CALayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.ca = nn.Sequential(
+            nn.Conv2d(channel, channel // 8, 1, padding=0, bias=True),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(channel // 8, channel, 1, padding=0, bias=True),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        y = self.avg_pool(x)
+        y = self.ca(y)
+        return x * y
+class Block(nn.Module):
+    def __init__(self, conv, dim, kernel_size, ):
+        super(Block, self).__init__()
+        self.conv1 = conv(dim, dim, kernel_size, bias=True)
+        self.act1 = nn.ReLU(inplace=True)
+        self.conv2 = conv(dim, dim, kernel_size, bias=True)
+        self.calayer = CALayer(dim)
+        self.palayer = PALayer(dim)
+
+    def forward(self, x):
+        res = self.act1(self.conv1(x))
+        res = res + x
+        res = self.conv2(res)
+        res = self.calayer(res)
+        res = self.palayer(res)
+        res += x
+        return res
+class Group(nn.Module):
+    def __init__(self, conv, dim, kernel_size, blocks):
+        super(Group, self).__init__()
+        modules = [Block(conv, dim, kernel_size) for _ in range(blocks)]
+        modules.append(conv(dim, dim, kernel_size))
+        self.gp = nn.Sequential(*modules)
+
+    def forward(self, x):
+        res = self.gp(x)
+        res += x
+        return res
+class FFA(nn.Module):
+    def __init__(self, gps, blocks, conv=default_conv):
+        super(FFA, self).__init__()
+        self.gps = gps
+        self.dim = 64
+        kernel_size = 3
+        pre_process = [conv(3, self.dim, kernel_size)]
+        assert self.gps == 3
+        self.g1 = Group(conv, self.dim, kernel_size, blocks=blocks)
+        self.g2 = Group(conv, self.dim, kernel_size, blocks=blocks)
+        self.g3 = Group(conv, self.dim, kernel_size, blocks=blocks)
+        self.ca = nn.Sequential(*[
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(self.dim * self.gps, self.dim // 16, 1, padding=0),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(self.dim // 16, self.dim * self.gps, 1, padding=0, bias=True),
+            nn.Sigmoid()
+        ])
+        self.palayer = PALayer(self.dim)
+
+        post_precess = [
+            conv(self.dim, self.dim, kernel_size),
+            conv(self.dim, 3, kernel_size)]
+
+        self.pre = nn.Sequential(*pre_process)
+        self.post = nn.Sequential(*post_precess)
+
+    def forward(self, x1):
+        x = self.pre(x1)
+        res1 = self.g1(x)
+        res2 = self.g2(res1)
+        res3 = self.g3(res2)
+        w = self.ca(torch.cat([res1, res2, res3], dim=1))
+        w = w.view(-1, self.gps, self.dim)[:, :, :, None, None]
+        out = w[:, 0, ::] * res1 + w[:, 1, ::] * res2 + w[:, 2, ::] * res3
+        out = self.palayer(out)
+        x = self.post(out)
+        return x + x1
+
+
+if __name__ == "__main__":
+    net = FFA(gps=3, blocks=19)
+    print(net)
+
 
 #---------------路由模块----------------
 #亮度与对比度调节
@@ -225,10 +403,15 @@ def smooth():
 def defogging():
     if request.method == 'POST':
         request_img = str(json.loads(request.values.get("data")))
-        imgData = Base64ToMat(request_img)  # base64转mat
-        result = Image_smoothing(imgData)  # 直方图均衡化
+        imgdata = base64.b64decode(request_img)#转为mat格式
+        file = open('去雾前.jpg', 'wb')
+        file.write(imgdata)
+        imgData = Image.open('去雾前.jpg')
+        result = Picture_defogging(imgData)  # 去雾
         image_base4 = MatToBase64(result)  # mat转base64
     return json.dumps(image_base4)
+
+
 
 if __name__ == '__main__':
     app.run(port=5001,debug=True)
